3061423b6212b047780a026d93bb17250ba6f6c5
Sergey Stepanov <mr.stepanov.sergey@gmail.com>
2012-12-03 18:52:02 +0300

    Release v.1.2.1
    minor fixes

--- src/com/epam/news/database/NewsDAOJPA.java
+++ src/com/epam/news/database/NewsDAOJPA.java
@@ -16,7 +16,7 @@ import com.epam.news.utils.EntityManagerFactoryWrapper;
  * @author Siarhei_Stsiapanau
  * 
  */
-public final class NewsDAOJPA implements INewsDao {
+public final class NewsDAOJPA extends ANewsDAO implements INewsDao {
     private EntityManagerFactoryWrapper entityManagerWrapper;
     private static final String DELETE_QUERY = "DELETE FROM News WHERE id IN(";
 
